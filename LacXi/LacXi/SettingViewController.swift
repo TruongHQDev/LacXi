53562c1ae5198d8ee279da3a8552dfdc1a9f7017
TruongHa <truonghq.dev@gmail.com>
2021-02-01 16:41:15 +0700

    add owner viewctrl

---
+++ LacXi/LacXi/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  LacXi
+//
+//  Created by AUTO SERVER on 01/02/2021.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
